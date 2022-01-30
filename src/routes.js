1004eede70828e330580b6e86d5ad5067f83bebc
fabiosdsilva <fabio12@live.com.br>
2022-01-30 16:19:23 -0300

    consumindo api library

--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 //HOME
-const home = require('./controllers/home/homeController')
-router.get('/', home.home)
+const home = require('./controllers/home/homeController');
+router.get('/', home.index);
 
 //READING
-const reading  = require('./controllers/home/homeController')
-router.get('/reading/:slug', reading.reading)
+const reading  = require('./controllers/home/homeController');
+router.get('/reading/:slug', reading.show);
 
 //SIGNUP
 
 
 //LOGIN
 const login = require('./controllers/login/loginController')
-router.get('/login', login.login)
-router.post('/login', login.loginAcess)
+const Login = new login()
+router.get('/login', Login.login)
+router.post('/login', Login.loginAcess)
 
 module.exports = router
