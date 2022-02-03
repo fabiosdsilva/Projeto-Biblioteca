6b01f86d76e2dcd40899ec522d88057dd4e93557
fabiosdsilva <fabiosds17@gmail.com>
2022-02-02 22:35:41 -0300

    add axios

--- src/controllers/login/loginController.js
+++ src/controllers/login/loginController.js
@@ -1,7 +1,20 @@
-exports.login = (req, res) =>{
-    res.render('login/login')
+const axios = require('axios');
+class LoginController {
+    async loginAcess(req, res) {
+        const { email, password } = req.body;
+        const data = { email, password };
+        try {
+          const request = await axios.post('http://localhost:8081/auth', data);
+          console.log(request.data);
+          return res.redirect('/');
+        } catch (error) {
+          return res.json(error);
+        }
+    }
+
+    login(req, res) {
+        res.render('login/login');
+    }
 }
 
-exports.loginAcess = (req, res) =>{
-    res.send('Acess')
-}
+module.exports = LoginController
